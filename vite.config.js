5bdbaebdf9ae8692eaa7aac6b7f77326a41a0edc
talltiny <hello@talltiny.com.au>
2025-05-14 20:25:41 +1000

    Update vite.config.js
    Fix vite config syntax error

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-javascriptimport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-}) 
+})
